85c46aa5f5a5e3e0105fc4ae61d2865bec990788
gulewei <gu.lewei@qq.com>
2020-04-08 22:33:33 +0800

    feat: 实现parseError

--- foundations-zh/src/index.ts
+++ foundations-zh/src/index.ts
@@ -8,5 +8,13 @@ export interface ErrorMessage {
 }
 
 export function parseError(err: Error): ErrorMessage {
-  // implement
+  // 正则图示： https://regexper.com/#%2F%28https%3F%3A%5C%2F%5C%2F%5B0-9%5C.%3A%5C%2Fa-zA-Z%5C-_%5D%2B%5C.js%29%3A%28%5Cd%2B%29%3A%28%5Cd%2B%29%2Fg
+  const reg = /(https?:\/\/[0-9\.:\/a-zA-Z\-_]+\.js):(\d+):(\d+)/g;
+  return {
+    message: err.message,
+    stack: err.stack
+      ? Array.from(err.stack.matchAll(reg))
+        .map(([, filename, line, column]) => ({ filename, line: Number(line), column: Number(column) }))
+      : []
+  }
 }
