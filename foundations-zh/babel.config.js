56e3d42b69410950943b399169d9025d74e743b5
gulewei <gu.lewei@qq.com>
2020-04-10 00:32:47 +0800

    build: 增加单元测试相关配置

---
+++ foundations-zh/babel.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                targets: {
+                    node: 'current',
+                },
+            },
+        ],
+        '@babel/preset-typescript',
+    ],
+    plugins: [
+        [
+            '@babel/plugin-transform-runtime',
+            {
+                'corejs': { version: 3, proposals: true },
+
+            }
+        ]
+    ],
+};
